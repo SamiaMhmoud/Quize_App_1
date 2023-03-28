710ec0e3431f03466df04252c7608c61614ea496
Samia Mahmoud Abd Elrazek <98880255+SamiaMhmoud@users.noreply.github.com>
2023-03-28 17:07:51 +0200

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -11,7 +11,61 @@ const start = document.querySelector(".start-btn"),
   optionsList = document.querySelector(".optins-list"),
   points = document.querySelector(".points"),
   timeLeft = document.querySelector(".time-sec"),
-  answer = document.querySelectorAll(".answer");
+  answer = document.querySelectorAll(".answer"),
+  scoreSpan = document.querySelector(".score-text span:first-child");
+  console.log(scoreSpan);
+  let questions = [
+    {
+      num: 1,
+      question: "What does HTML stand for?",
+      answer: "Hyper Text Markup languge",
+      options: [
+        "Hyper Text Preprocessor",
+        "Hyper Text Markup languge",
+        "Hyper Text Multiple languge",
+        "Hyper Text Tool Multi languge",
+      ],
+    },
+    {
+      num: 2,
+      question: "Why We Use < br> Element?",
+      answer: "To Add Breakline",
+      options: [
+        "To Make Text Bold",
+        "To Make Horizontal Line",
+        "To Add Breakline",
+        "To Make Text Italic",
+      ],
+    },
+    {
+      num: 3,
+      question: "Is <img> Element Has Attribute href?",
+      answer: "No Its For Anchor Tag < a >",
+      options: [
+        "Yes",
+        "No Its For Anchor Tag < a >",
+        "All Element Has This Attribute",
+        "Answer 1 And 3 Are Right",
+      ],
+    },
+    {
+      num: 4,
+      question: "What Is The Element Thats Not Exists In Html5 Semantics?",
+      answer: "< blockquote >",
+      options: ["< article >", "< section >", "< blockquote >", "< aside >"],
+    },
+    {
+      num: 5,
+      question: "In Html Can We Use This Way To Add Attributes?",
+      answer: "All Answer Is Right",
+      options: [
+        "< div class='class-name'> ",
+        "< div class=class-name> ",
+        '< div class="class-name"> ',
+        `All Answer Is Right`,
+      ],
+    },
+  ];
 // If Start Quiz button clicked
 start.onclick = () => {
   rulesBox.classList.add("active");
